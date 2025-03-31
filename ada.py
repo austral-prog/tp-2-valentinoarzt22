a28bec2656cc2ba630b452b46383b5aab2027ef5
valentinoarzt22 <vrarzt@mail.austral.edu.ar>
2025-03-31 12:03:09 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,3 +1,13 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
+    lower_name = first_name.lower() + ' ' + last_name.lower()
+    capitalize_name = first_name.capitalize() + ' ' + last_name.capitalize()
+    upper_name = first_name.upper() + ' ' + last_name.upper()
+    space_name = ' ' * 4 + first_name.lower() + ' ' + last_name.lower()
+    print(lower_name)
+    print(capitalize_name)
+    print(upper_name)
+    print(space_name)
+
+ada()
